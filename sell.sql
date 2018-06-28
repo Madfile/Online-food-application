be9f8401f7769fa7bfcd085bfb88228d922e8080
AlienGray <1486206941@qq.com>
2018-06-28 14:24:59 +1000

    Update sell.sql

--- sell.sql
+++ sell.sql
@@ -31,6 +31,8 @@ create table `order_master` (
 	`order_amount` decimal(8,2) not null comment '订单总金额',
 	`order_status` tinyint(3) not null default '0' comment '订单状态，默认0新下单',
 	`pay_status` tinyint(3) not null default '0' comment '支付状态，默认0未支付',
+	`create_time` timestamp not null default current_timestamp comment '创建时间',
+	`update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
 	primary key (`order_id`),
 	key `idx_buyer_openid` (`buyer_openid`)
 )  comment '订单表';
